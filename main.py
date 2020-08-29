13bd91ed9813eb6cfec748cd92baf3bab14fa62f
Luke Kelly <luke.kelly141295@gmail.com>
2020-08-29 16:31:03 +0100

    black formatter set

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+"""This is a test main file
+"""
 import numpy as np
 
+
 def zeros_function(a, b):
-	array = np.zeros((a, b))
-	print(array)
+    """This is a sample function
+
+	Args:
+		a (int): any integer
+		b (int): any integer
+	"""
+    array = np.zeros((a, b))
+    print(array)
+
 
 if __name__ == "__main__":
+    """This is test main function
+	"""
     zeros_function(2, 2)
